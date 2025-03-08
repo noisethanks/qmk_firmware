77eb15d4b25a3a8f2054b06fec6ef6b86d5a0c30
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2025-03-08 12:53:40 -0800

    Moved - and = to same key, different layers. Moved GRV to same key as ESC. Other minor enhancement. Added MEH mod.

--- keyboards/keebio/iris/keymaps/abhi/rules.mk
+++ keyboards/keebio/iris/keymaps/abhi/rules.mk
@@ -4,7 +4,7 @@
 BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = no       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
+CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
@@ -16,3 +16,4 @@ ENCODER_ENABLE = no
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_KEYPRESSES = yes
 LTO_ENABLE = yes
+# KEY_OVERRIDE_ENABLE = yes
